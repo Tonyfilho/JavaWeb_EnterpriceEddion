6393a678a493c1253b0c4d92a3ddccbc7ea3b627
tonyfilho <oficinadosdrones@gmail.com>
2024-07-23 11:37:30 +0100

    sendRedirect no lugar do forward

--- JavaWeb/src/main/java/com/tony/javaservlets/ServletAction.java
+++ JavaWeb/src/main/java/com/tony/javaservlets/ServletAction.java
@@ -2,7 +2,7 @@ package com.tony.javaservlets;
 
 
 
-import jakarta.servlet.RequestDispatcher;
+//import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -13,13 +13,25 @@ public class ServletAction extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // redirecionamento com forward
-        request.setAttribute("OK", "O pedido passou por ServletAction");
-        RequestDispatcher rd = request.getRequestDispatcher("ServletAlvo01");
-        rd.forward(request, response);
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+        //redirecionamento com forward
+        //request.setAttribute("OK", "O pedido passou por ServletAction");
+        //RequestDispatcher rd = request.getRequestDispatcher("ServletAlvo01");
+        //rd.forward(request, response);
+        //redirecionamento com sendRedirect()
+        
+        // redirecionamento com sendRedirect()
+        //response.sendRedirect("ServletAlvo01");
+    	
+    	/*ou*/
+    	/*Com este tipo de redirecionamento não é possível enviar o objeto 
+    	 * HttpServletRequest à segunda servlet, pelo que a única maneira 
+    	 * de passar parâmetros é usando o URL do redirecionamento, como 
+    	 * mostra a alteração apresentada no exemplo abaixo, feita no método doGet() 
+    	 * de ServletAction:**/
+        response.sendRedirect("ServletAlvo01?"+request.getQueryString());
     }
-
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         doGet(request, response);
     }
